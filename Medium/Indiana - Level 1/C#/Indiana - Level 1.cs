d8aa7b13c35d7dc59ca81fe6e07c780d107746d4
Roman <ghostxvipart@gmail.com>
2016-03-27 23:12:27 +0700

    Added new and improved earlier solutions

--- Medium/Indiana - Level 1/C#/Indiana - Level 1.cs
+++ Medium/Indiana - Level 1/C#/Indiana - Level 1.cs
@@ -2,106 +2,81 @@
 
 class Player
 {
-    static void Main(string[] args)
+    static void Main()
     {
         string[] inputs = Console.ReadLine().Split(' ');
-        int numberOfColumns = int.Parse(inputs[0]); // number of columns.
-        int numberOfRows = int.Parse(inputs[1]); // number of rows.
+        int numberOfColumns = int.Parse(inputs[0]);
+        int numberOfRows    = int.Parse(inputs[1]);
 
         int[,] map = new int[numberOfRows, numberOfColumns];
         for (int i = 0; i < numberOfRows; i++)
         {
-            string line = Console.ReadLine(); // represents a line in the grid and contains W integers. Each integer represents one room of a given type.
-            string[] row = line.Split();
+            string[] entranceRoomDirection = Console.ReadLine().Split();
 
             for (int j = 0; j < numberOfColumns; j++)
-            {
-                map[i, j] = int.Parse(row[j]);
-            }
+                map[i, j] = int.Parse(entranceRoomDirection[j]);
         }
-        int EX = int.Parse(Console.ReadLine()); // the coordinate along the X axis of the exit (not useful for this first mission, but must be read).
+        int exitCoordinates = int.Parse(Console.ReadLine());
 
         while (true)
         {
             inputs = Console.ReadLine().Split(' ');
-            int XI = int.Parse(inputs[0]);
-            int YI = int.Parse(inputs[1]);
-            string position = inputs[2];
+            int currentXPosition    = int.Parse(inputs[0]);
+            int currentYPosition    = int.Parse(inputs[1]);
+            string currentDirection = inputs[2];
 
-            int X = 0; int Y = 0;
+            int nextXPosition = currentXPosition; int nextYPosition = currentYPosition;
 
-            switch (map[YI, XI])
+            switch (map[currentYPosition, currentXPosition])
             {
                 case 0:
                 case 1:
-                    Y++;
+                    nextYPosition++;
                     break;
-
                 case 2:
-                    X = (position[0] == 'L') ? 1 : -1;
+                    if (currentDirection[0] == 'L')
+                        nextXPosition++; 
+                    else
+                        nextXPosition--;
                     break;
-
                 case 3:
-                    Y++;
+                    nextYPosition++;
                     break;
-
                 case 4:
-                    switch (position[0])
-                    {
-                        case 'T':
-                            X--;
-                            break;
-                        case 'R':
-                            Y++;
-                            break;
-                    }
+                    if (currentDirection[0] == 'T')
+                        nextXPosition--;
+                    else
+                        nextYPosition++;
                     break;
-
                 case 5:
-                    switch (position[0])
-                    {
-                        case 'T':
-                            X++;
-                            break;
-                        case 'L':
-                            Y++;
-                            break;
-                    }
+                    if (currentDirection[0] == 'T')
+                        nextXPosition++;
+                    else
+                        nextYPosition++;
                     break;
-
                 case 6:
-                    switch (position[0])
-                    {
-                        case 'L':
-                            X++;
-                            break;
-
-                        case 'R':
-                            X--;
-                            break;
-                    }
+                    if (currentDirection[0] == 'L')
+                        nextXPosition++;
+                    else
+                        nextXPosition--;
                     break;
-
                 case 7:
                 case 8:
                 case 9:
-                    Y++;
+                    nextYPosition++;
                     break;
-
                 case 10:
-                    X--;
+                    nextXPosition--;
                     break;
-
                 case 11:
-                    X++;
+                    nextXPosition++;
                     break;
-
                 case 12:
                 case 13:
-                    Y++;
+                    nextYPosition++;
                     break;
             }
-            Console.WriteLine((XI + X) + " " + (YI + Y)); // One line containing the X Y coordinates of the room in which you believe Indy will be on the next turn.
+            Console.WriteLine(nextXPosition + " " + nextYPosition);
         }
     }
 }
