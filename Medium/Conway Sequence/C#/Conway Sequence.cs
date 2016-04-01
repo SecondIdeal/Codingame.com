d899d323519c25765db6d1566d569c904858dae2
Roman <ghostxvipart@gmail.com>
2016-04-01 23:32:10 +0700

    Added new and improved earlier solutions

---
+++ Medium/Conway Sequence/C#/Conway Sequence.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    static void Main()
+    {
+        string nextLine = Console.ReadLine();
+        int numberOfLineToOutPut = int.Parse(Console.ReadLine()); // The index of the first line is 1.
+
+        for (int i = 1; i < numberOfLineToOutPut; i++)
+        {
+            string previousLine = nextLine;
+
+            List<string> nextLineList = new List<string>();
+            string[] previousLineArray = previousLine.Split(' ');
+
+            for (int j = 0; j < previousLineArray.Length; j++)
+            {
+                if (nextLineList.Count == 0)
+                {
+                    nextLineList.Add("1");
+                    nextLineList.Add(previousLineArray[j]);
+                }
+                else
+                {
+                    if (previousLineArray[j] == nextLineList[nextLineList.Count - 1])
+                        nextLineList[nextLineList.Count - 2] = (int.Parse(nextLineList[nextLineList.Count - 2]) + 1).ToString();
+                    else
+                    {
+                        nextLineList.Add("1");
+                        nextLineList.Add(previousLineArray[j]);
+                    }
+                }
+            }
+            nextLine = string.Join(" ", nextLineList.ToArray());
+        }
+        Console.WriteLine(nextLine);
+    }
+}
---
+++ Medium/Conway Sequence/Python/Conway Sequence.py
@@ -0,0 +1,21 @@
+﻿next_line = raw_input()
+number_of_line_to_output = int(raw_input()) # The index of the first line is 1.
+
+for i in range (1, number_of_line_to_output):
+    previous_line = next_line
+    
+    next_line_list = []
+    previous_line_array = str(previous_line).replace("[", "").replace("]", "").replace(",", "").replace("\'", "").split(" ")
+
+    for j in range(0, len(previous_line_array)):
+        if (len(next_line_list) == 0):
+            next_line_list.append("1")
+            next_line_list.append(previous_line_array[j])
+        else:
+            if (previous_line_array[j] == next_line_list[len(next_line_list) - 1]):
+                next_line_list[len(next_line_list) - 2] = str(int(next_line_list[len(next_line_list) - 2]) + 1)
+            else:
+                next_line_list.append("1")
+                next_line_list.append(previous_line_array[j])
+    next_line = next_line_list
+print(str(next_line).replace("[", "").replace("]", "").replace(",", "").replace("\'", ""))
