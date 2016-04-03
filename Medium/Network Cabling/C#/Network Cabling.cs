71bc6c8e319917861913911f8be703312cfaadfd
Roman <ghostxvipart@gmail.com>
2016-04-03 22:23:30 +0700

    Added new and improved earlier solutions

---
+++ Medium/Network Cabling/C#/Network Cabling.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+internal class Solution
+{
+    private static void Main()
+    {
+        List<int> xCoordinates = new List<int>();
+        List<int> yCoordinates = new List<int>();
+
+        int numberOfBuildings = int.Parse(Console.ReadLine());
+        for (int i = 0; i < numberOfBuildings; i++)
+        {
+            string[] coordinatesOfTheBuildings = Console.ReadLine().Split(' ');
+            xCoordinates.Add(int.Parse(coordinatesOfTheBuildings[0]));
+            yCoordinates.Add(int.Parse(coordinatesOfTheBuildings[1]));
+        }
+        
+        yCoordinates.Sort();
+        long minimalCableLength = xCoordinates.Max() - xCoordinates.Min();
+        int cableYPosition      = yCoordinates[numberOfBuildings % 2 == 0 ? numberOfBuildings / 2 : (numberOfBuildings - 1) / 2];
+
+        for (int i = 0; i < numberOfBuildings; i++)
+            minimalCableLength = minimalCableLength + Math.Abs(yCoordinates[i] - cableYPosition);
+
+        Console.WriteLine(minimalCableLength);
+    }
+}
