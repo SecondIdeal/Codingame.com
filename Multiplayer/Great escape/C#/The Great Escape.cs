764419a1d7e2d7431646f250ee6178008b133c3b
Roman <ghostxvipart@gmail.com>
2016-05-04 17:31:09 +0700

    Added new and improved earlier solutions

--- Multiplayer/Great escape/C#/The Great Escape.cs
+++ Multiplayer/Great escape/C#/The Great Escape.cs
@@ -7,16 +7,16 @@ using System.Collections.Generic;
 
 class Player
 {
-    static void Main(string[] args)
+    static void Main()
     {
         string[] inputs = Console.ReadLine().Split(' ');
-        int widthOfBoard = int.Parse(inputs[0]); // width of the board
-        int heightOfBoard = int.Parse(inputs[1]); // height of the board
-        int playerCount = int.Parse(inputs[2]); // number of players (2 or 3)
+        int widthOfBoard = int.Parse(inputs[0]); 
+        int heightOfBoard = int.Parse(inputs[1]); 
+        int numberOfPlayers = int.Parse(inputs[2]); // number of players (2 or 3)
         int myId = int.Parse(inputs[3]); // id of my player (0 = 1st player, 1 = 2nd player, ...)
 
-        int counterForStartPositions = 0;
-        string[] playerStartPositions = new string[playerCount];
+        int counterForStartPositions = 0; // На первом ходу необходимо собрать начальные позиции игроков.
+        string[] playerStartPositions = new string[numberOfPlayers];
 
         while (true)
         {
@@ -25,18 +25,18 @@ class Player
             string[,] wallsMapH = new string[widthOfBoard, heightOfBoard];
 
             // Текущее местоположение каждого из игроков
-            int[] allPlayersX = new int[playerCount];
-            int[] allPlayersY = new int[playerCount];
+            int[] allPlayersX = new int[numberOfPlayers];
+            int[] allPlayersY = new int[numberOfPlayers];
 
             // По количеству стен = -1 мы можем определить, что игрок больше не играет, это необходимо, чтобы правильно расчитывать сколько кому осталось до финиша
-            int[] playerWallsLeft = new int[playerCount];
+            int[] playerWallsLeft = new int[numberOfPlayers];
 
-            for (int i = 0; i < playerCount; i++)
+            for (int i = 0; i < numberOfPlayers; i++)
             {
                 inputs = Console.ReadLine().Split(' ');
-                int playerX = int.Parse(inputs[0]); // x-coordinate of the player
-                int playerY = int.Parse(inputs[1]); // y-coordinate of the player
-                int wallsLeft = int.Parse(inputs[2]); // number of walls available for the player
+                int playerX = int.Parse(inputs[0]); 
+                int playerY = int.Parse(inputs[1]); 
+                int wallsLeft = int.Parse(inputs[2]);
 
                 // Запишем текущее местоположение игрока
                 allPlayersX[i] = playerX;
@@ -45,22 +45,17 @@ class Player
                 playerWallsLeft[i] = wallsLeft;
 
                 // Определим стартовую сторону каждого игрока 
-                if (i == 0)
-                    playerStartPositions[i] = "L";
-                else if (i == 1)
-                    playerStartPositions[i] = "R";
-                else
-                    playerStartPositions[i] = "U";             
+                playerStartPositions[i] = (i == 0) ? "L" : (i == 1) ? "R" : "U";          
             }
 
-            int wallsOnBoard = int.Parse(Console.ReadLine()); // number of walls on the board
+            int numberOfWallsOnBoard = int.Parse(Console.ReadLine()); // number of walls on the board
 
-            for (int i = 0; i < wallsOnBoard; i++)
+            for (int i = 0; i < numberOfWallsOnBoard; i++)
             {
                 inputs = Console.ReadLine().Split(' ');
-                int wallX = int.Parse(inputs[0]); // x-coordinate of the wall
-                int wallY = int.Parse(inputs[1]); // y-coordinate of the wall
-                string wallOrientation = inputs[2]; // wall orientation ('H' or 'V')
+                int wallX = int.Parse(inputs[0]); 
+                int wallY = int.Parse(inputs[1]); 
+                string wallOrientation = inputs[2]; // 'H' or 'V'
 
                 // Разместим стену на карте             
                 if (wallOrientation == "H")
@@ -76,15 +71,15 @@ class Player
             }
 
             // Если стен нет на карте, то и путь свободен
-            bool pathIsClear = isPathClear(wallsMapV, wallsMapH, playerStartPositions, wallsOnBoard, myId, allPlayersX[myId], allPlayersY[myId]);
+            bool pathIsClear = isPathClear(wallsMapV, wallsMapH, playerStartPositions, numberOfWallsOnBoard, myId, allPlayersX[myId], allPlayersY[myId]);
 
             // Кто первый успеет дойти  
-            int[] pathLenghtsForPlayers = calculatePathsLenghts(allPlayersX, allPlayersY, playerWallsLeft, playerStartPositions, playerCount, myId, widthOfBoard, heightOfBoard);
+            int[] pathLenghtsForPlayers = calculatePathsLenghts(allPlayersX, allPlayersY, playerWallsLeft, playerStartPositions, numberOfPlayers, myId, widthOfBoard, heightOfBoard);
 
             int targetEnemy = 0;
             int targetEnemyPathLenght = int.MaxValue;
 
-            for (int i = 0; i < playerCount; i++)
+            for (int i = 0; i < numberOfPlayers; i++)
             {
                 if (i != myId && playerWallsLeft[i] != -1)
                     if (pathLenghtsForPlayers[i] < targetEnemyPathLenght)
@@ -93,9 +88,9 @@ class Player
             }
             bool shoot = (pathLenghtsForPlayers[myId] > targetEnemyPathLenght && targetEnemy != myId) ? true : false;
 
-            string newDirection = directionToExit(wallsMapV, wallsMapH, wallsOnBoard, playerStartPositions, pathIsClear, allPlayersX, allPlayersY, heightOfBoard, widthOfBoard, myId);
-            bool enemyPathIsClear = isPathClear(wallsMapV, wallsMapH, playerStartPositions, wallsOnBoard, targetEnemy, allPlayersX[targetEnemy], allPlayersY[targetEnemy]);
-            string newWallPosition = (playerWallsLeft[myId] != 0) ? calculateNewWallPositionAndDirection(wallsMapV, wallsMapH, allPlayersX, allPlayersY, playerStartPositions, wallsOnBoard, targetEnemy, widthOfBoard, heightOfBoard) : "";
+            string newDirection = directionToExit(wallsMapV, wallsMapH, numberOfWallsOnBoard, playerStartPositions, pathIsClear, allPlayersX, allPlayersY, heightOfBoard, widthOfBoard, myId);
+            bool enemyPathIsClear = isPathClear(wallsMapV, wallsMapH, playerStartPositions, numberOfWallsOnBoard, targetEnemy, allPlayersX[targetEnemy], allPlayersY[targetEnemy]);
+            string newWallPosition = (playerWallsLeft[myId] != 0) ? calculateNewWallPositionAndDirection(wallsMapV, wallsMapH, allPlayersX, allPlayersY, playerStartPositions, numberOfWallsOnBoard, targetEnemy, widthOfBoard, heightOfBoard) : "";
 
             if (pathIsClear && !shoot)
                 Console.WriteLine(newDirection);
@@ -106,7 +101,7 @@ class Player
                 else if (!enemyPathIsClear)
                     Console.WriteLine(newDirection);
                 else
-                    Console.WriteLine("Module pathIsClear && shoot.");
+                    Console.WriteLine("Error in module pathIsClear && shoot.");
             }
             else if (!pathIsClear && !shoot)
                 Console.WriteLine(newDirection);
@@ -117,17 +112,17 @@ class Player
                 else if (!enemyPathIsClear)
                     Console.WriteLine(newDirection);
                 else
-                    Console.WriteLine("Module !pathIsClear && shoot.");
+                    Console.WriteLine("Error in module !pathIsClear && shoot.");
             }
-            counterForStartPositions++; // Необходима для самого первого хода, чтобы собрать начальные позиции игроков.
+            counterForStartPositions++; 
         }
     }
 
-    static bool isPathClear(string[,] wallsMapV, string[,] wallsMapH, string[] playerStartPositions, int wallsOnBoard, int playerId, int playerXPos, int playerYPos)
+    static bool isPathClear(string[,] wallsMapV, string[,] wallsMapH, string[] playerStartPositions, int numberOfWallsOnBoard, int playerId, int playerXPos, int playerYPos)
     {
         bool pathIsClear = true;
 
-        if (wallsOnBoard != 0)
+        if (numberOfWallsOnBoard != 0)
         {
             if (playerStartPositions[playerId] == "L")
                 pathIsClear = (wallsMapV[playerXPos + 1, playerYPos] == "V") ? false : true;
@@ -141,12 +136,12 @@ class Player
         return pathIsClear;
     }
 
-    static int[] calculatePathsLenghts(int[] allPlayersX, int[] allPlayersY, int[] playerWallsLeft, string[] playerStartPositions, int playerCount, int myId, int widthOfBoard, int heightOfBoard)
+    static int[] calculatePathsLenghts(int[] allPlayersX, int[] allPlayersY, int[] playerWallsLeft, string[] playerStartPositions, int numberOfPlayers, int myId, int widthOfBoard, int heightOfBoard)
     {
         // Высчитываем чистую длину пути. Например, решаем удивить игрока стеной, ибо он быстрее нас дотопает, но перед ним стена нужного направления, тогда ничего не делаем. 
-        int[] pathLenghtsForPlayers = new int[playerCount];
+        int[] pathLenghtsForPlayers = new int[numberOfPlayers];
 
-        for (int i = 0; i < playerCount; i++)
+        for (int i = 0; i < numberOfPlayers; i++)
         {
             if (playerStartPositions[i] == "L")
                 pathLenghtsForPlayers[i] = (widthOfBoard - 1) - allPlayersX[i];
@@ -165,38 +160,18 @@ class Player
         // Делаем нас менее агрессивными...
         if (myId == 0)
         {
-            if (playerCount == 2)
+            if (numberOfPlayers == 2)
                 pathLenghtsForPlayers[1]++;
-            else //if (playerCount == 3)
+            else //if (numberOfPlayers == 3)
             {
                 pathLenghtsForPlayers[1]++;
                 pathLenghtsForPlayers[2]++; //= pathLenghtsForPlayers[2] + 2;
             }
         }
-        else if (myId == 1)
-        {
-            //pathLenghtsForPlayers[0]++;
-            //pathLenghtsForPlayers[2]++;
-        }
-        else if (myId == 2)
-        {
-            //pathLenghtsForPlayers[0]++;
-            //pathLenghtsForPlayers[1]++; 
-        }
-
-        // Ситуация: Два игрока, я второй, я иду справа налево, он - слева направо, он успевает дойти первым, так как начинает ход первым.
-        // Оказалось, что нет необходимости, тебе даются текущего местоположения, если походил уже - то походил.
-        //if (myId == 1) // То есть я справа
-        //    pathLenghtsForPlayers[0]--;
-        //if (myId == 2) // Я сверху, хожу последним
-        //{
-        //    pathLenghtsForPlayers[0] = pathLenghtsForPlayers[0] - 2;
-        //    pathLenghtsForPlayers[1]--;
-        //}
         return pathLenghtsForPlayers;
     }
 
-    static string calculateNewWallPositionAndDirection(string[,] wallsMapV, string[,] wallsMapH, int[] allPlayersX, int[] allPlayersY, string[] playerStartPositions, int wallsOnBoard, int targetEnemy, int widthOfBoard, int heightOfBoard)
+    static string calculateNewWallPositionAndDirection(string[,] wallsMapV, string[,] wallsMapH, int[] allPlayersX, int[] allPlayersY, string[] playerStartPositions, int numberOfWallsOnBoard, int targetEnemy, int widthOfBoard, int heightOfBoard)
     {
         // Cюда мы попали потому что, перед врагом на пути нет стены нужного направления - надо её поставить.
         string newWallOrientation = (playerStartPositions[targetEnemy] == "U") ? "H" : "V";
@@ -207,6 +182,7 @@ class Player
         int enemyPosX = allPlayersX[targetEnemy];
         int enemyPosY = allPlayersY[targetEnemy];
 
+        // Очень плохой код...
         if (playerStartPositions[targetEnemy] == "L")
         {
             if (enemyPosY != (heightOfBoard - 1))
@@ -343,18 +319,16 @@ class Player
             }
         }
         else
-            Console.WriteLine("Module calculateNewWallPositionAndDirection.");
+            Console.WriteLine("Error in module calculateNewWallPositionAndDirection.");
 
         // Если  стена не влезет, то мы отправим пустую строку, что будет означать, что надо просто сходить далее.
         return (newWallXPos == int.MaxValue && newWallYPos == int.MaxValue) ? "" : (newWallXPos.ToString() + " " + newWallYPos.ToString() + " " + newWallOrientation);
     }
 
-    static string directionToExit(string[,] wallsMapV, string[,] wallsMapH, int wallsOnBoard, string[] playerStartPositions, bool pathIsClear, int[] allPlayersX, int[] allPlayersY, int heightOfBoard, int widthOfBoard, int myId)
+    static string directionToExit(string[,] wallsMapV, string[,] wallsMapH, int numberOfWallsOnBoard, string[] playerStartPositions, bool pathIsClear, int[] allPlayersX, int[] allPlayersY, int heightOfBoard, int widthOfBoard, int myId)
     {
         // Много кода, плохого кода, надо всё поправить.
-        
-        // Когда pathLenghtUp == pathLenghtDown всему придет хана, то что уже написано -  не решит эту проблему.
-        
+
         // Проверить не выходим ли мы за границы карты. Например, мы в самой верхней точке, явно стену обойти ещё выше не сможем.
         string desiredDirection = "";
 
@@ -367,7 +341,7 @@ class Player
 
             if (playerStartPositions[myId] == "L")
             {
-                // Здесь узкое место! Если например, справа стена, сверху, например, край, то, иди, мол, вниз, а там горизонтальная стена...Правильно было бы пойти назад.
+                // Здесь узкое место! Если например, справа - стена, сверху - край, то, иди - вниз, а там горизонтальная стена...Правильно было бы пойти назад.
                 if (allPlayersY[myId] == 0)
                     desiredDirection = "DOWN";
                 else if (allPlayersY[myId] == (heightOfBoard - 1))
